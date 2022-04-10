818330f0be64d4c1c8df0cbd10370fe8b1db848d
stellalc7 <stellalc7@gmail.com>
2022-04-10 17:16:17 -0400

    fint

--- pages/index.js
+++ pages/index.js
@@ -16,17 +16,34 @@ export default function Home() {
 
       <main className={styles.main}>
         <div className={styles.about}>
+        <h2><span>about</span></h2>
+          <div className={styles.info}>
+            stella l. choi<br></br>
+            frontend developer<br></br>
+            NY
+          </div>
           <Image
             src={me}
             alt="Picture of the author"
-            width={150}
-            height={150}
+            width={170}
+            height={170}
             className={styles.portrait}
           />
-          <div className={styles.info}>
-            <span>stella lina choi</span><br></br>
-            frontend developer
+        </div>
+
+
+        <div className={styles.work}>
+          <h2><span>projects</span></h2>
+          <div className={styles.projects}>
+            <div>BEAT C♡NNECTI♡N</div>
+            <div>My Aunt&apos;s Kimchi Rules</div>
+            <div>omakase</div>
+            <div>SynthGarden</div>
           </div>
+          {/* <div className={styles.projectDetails}>
+            A full stack, interactive site encouraging exploration. Discover streams, top national headlines, weather, and time.<br></br>
+            Doubles as passive tech: the UI/UX is minimal, visual hierarchy is reconsidered in response to the user&apos;s interactions, errors are handled without introducing new elements, and there&apos;s a subtle rotation on the canvas.
+          </div> */}
         </div>
       </main>
 
