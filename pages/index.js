5b2cd9e06ba49aa30c2480c61d760994032ab473
stellalc7 <stellalc7@gmail.com>
2022-05-14 22:36:30 -0400

    pink

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { useState } from 'react'
+// import { useState } from 'react'
 
 import me from '../images/me.png'
-import Project from './components/Project'
+// import Project from './components/Project'
 
 export default function Home() {
-  const [project, setProject] = useState('');
+  // const [project, setProject] = useState('');
   
   return (
     <div className={styles.container}>
@@ -19,40 +19,45 @@ export default function Home() {
 
       <main className={styles.main}>
         <div className={styles.about}>
-        <h2><span>about</span></h2>
+        <h2><span className={styles.dot}></span> <span className={styles.dot}></span> stella l choi <span className={styles.dot}></span> <span className={styles.dot}></span></h2>
           <div className={styles.info}>
-            stella l. choi<br></br>
+            is a web3 frontend developer based in US▲.<br></br><br></br>
+            previ♡usly, she pursued atm⁂spheric science, and taught. the national science f☻undation funded her master&apos;s research harm〄nizing social media and rainfall data.<br></br><br></br>
             {/* <div className={project === 'more' ? styles.active : null} onMouseOver={() => setProject('more')}>stella l. choi</div> */}
-            developer<br></br>
-          </div>
+            don&apos;t hesitate ⟿ to never make <a
+              href="mailto:stella.choi@hyperisland.se"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              c•ntact
+            </a>.
+          </div><br></br>
           <Image
-            src={me}
-            alt="Picture of the author"
-            width={170}
-            height={170}
-            className={styles.portrait}
-          />
+              src={me}
+              alt="me"
+              width={170}
+              height={170}
+              className={styles.portrait}
+            />
         </div>
 
-        <div className={styles.work}>
+        {/* <div className={styles.work}>
           <h2><span>projects</span></h2>
           <div className={styles.projects}>
             <div className={styles.projectList}>
               <div className={project === 'beat' ? styles.active : null} onMouseOver={() => setProject('beat')}>BEAT C♡NNECTI♡N</div>
               <div className={project === 'kimchi' ? styles.active : null} onMouseOver={() => setProject('kimchi')}>My Aunt&apos;s Kimchi Rules</div>
-              {/* <div className={project === 'omakase' ? styles.active : null} onMouseOver={() => setProject('omakase')}>omakase</div> */}
               <div className={project === 'synth' ? styles.active : null} onMouseOver={() => setProject('synth')}>SynthGarden</div>
-              {/* <div className={project === 'bike' ? styles.active : null} onMouseOver={() => setProject('bike')}>Bikes, Beats, and Beers</div> */}
-              {/* <div className={project === 'museo' ? styles.active : null} onMouseOver={() => setProject('museo')}>museum</div><br></br><br></br><br></br><br></br> */}
+              <div className={project === 'bike' ? styles.active : null} onMouseOver={() => setProject('bike')}>Bikes, Beats, and Beers</div>
             </div>
             <div className={styles.projectDetails}>
               <Project project={project} />
             </div>
           </div>
-        </div>
+        </div> */}
       </main>
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="mailto:stella.choi@hyperisland.se"
           target="_blank"
@@ -60,7 +65,7 @@ export default function Home() {
         >
           e•mail
         </a>
-      </footer>
+      </footer> */}
     </div>
   )
 }
